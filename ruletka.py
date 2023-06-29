e264dc7de0957d945bf665a4a638db5ee81f0211
panpiton <138117566+panpiton@users.noreply.github.com>
2023-06-29 16:59:03 +0600

    Add files via upload

---
+++ ruletka.py
@@ -0,0 +1,19 @@
+a1 = int(input())
+b1 = int(input())
+a2 = int(input())
+b2 = int(input())
+if a2<a1 and b2<b1 :
+    a1=a2
+    a2=a1
+    b1=b2
+    b2=b1
+elif a1< a2 < b1:
+    print(a2, b1) 
+elif a2< b1< b2:
+    print(a1, b2) 
+elif a2==b1:
+    print(a2)
+elif b2==a1:
+    print(b2)
+elif  a2>b1:
+    print('пустое множество')
